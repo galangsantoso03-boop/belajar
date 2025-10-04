52d9e7c08a701eb613ad2cc5e1bc92a043da1516
galangsantoso03-boop <galangsantoso03@gmail.com>
2025-10-04 17:09:19 +0700

    Update db_manager.py

--- belajar/db_manager.py
+++ belajar/db_manager.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
 class DatabaseManager:
-    def __init__(self, host, user, password, database):
+    def __init__(self, host, user, password ,database):
         self.host = host
         self.user = user
         self.password = password
-        self.database = database
+        self.database =database
         self.connection = None
         self.cursor = None
 
@@ -18,41 +18,54 @@ class DatabaseManager:
                 database=self.database
             )
             self.cursor = self.connection.cursor(buffered=True)
+            print("Koneksi ke database berhasil.")
         except mysql.connector.Error as e:
-            print("❌ Error:", e)
-
+            print(f"Gagal koneksi ke database: {e}")
+    
     def login(self, username, password):
         sql = "SELECT * FROM users WHERE username = %s AND password = %s"
         self.cursor.execute(sql, (username, password))
         result = self.cursor.fetchone()
-        return result is not None
-    
+        return result is not None 
+
     def create(self, name, age):
         sql = "INSERT INTO data (name, age) VALUES (%s, %s)"
         self.cursor.execute(sql, (name, age))
         self.connection.commit()
-        print("✅ Data inserted successfully")
+        print("DATA TELAH BERHASIL DI BUAT.")
 
     def read(self):
         self.cursor.execute("SELECT * FROM data")
         results = self.cursor.fetchall()
-        for row in results:
-            print(row)
+        if results:
+            print("=== DATA ===")
+            for row in results:
+                print(f"ID: {row[0]}, Name: {row[1]}, Age: {row[2]}")
+        else:
+            print("Tidak ada data ditemukan.")
 
     def update(self, data_id, name, age):
-        sql = "UPDATE data SET name=%s, age=%s WHERE id=%s"
+        sql = "UPDATE data SET name = %s, age = %s WHERE id = %s"
         self.cursor.execute(sql, (name, age, data_id))
         self.connection.commit()
-        print("✅ Data updated successfully")
-
+        if self.cursor.rowcount:
+            print("DATA BERHASIL DI UPDATE.")
+        else:
+            print("DATA TIDAK DITEMUKAN.")
+    
     def delete(self, data_id):
-        sql = "DELETE FROM data WHERE id=%s"
+        sql = "DELETE FROM data WHERE id = %s"
         self.cursor.execute(sql, (data_id,))
         self.connection.commit()
-        print("✅ Data deleted successfully")
+        if self.cursor.rowcount:
+            print("DATA BERHASIL DI HAPUS. ")
+        else:
+            print("DATA TIDAK DITEMUKAN.")
 
     def close(self):
         if self.cursor:
             self.cursor.close()
         if self.connection:
-            self.connection.close() 
+            self.connection.close()
+            print("Koneksi ke database ditutup.")
+            
