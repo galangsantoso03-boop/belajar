3194d020121759d69767eb5674827e9ee408baff
galangsantoso03-boop <galangsantoso03@gmail.com>
2025-10-06 05:32:32 +0700

    Update main.py

--- belajar/main.py
+++ belajar/main.py
@@ -1,59 +1,62 @@
 from db_manager import DatabaseManager
 
 def main():
-    db = DatabaseManager("127.0.0.1", "root", "", "test_db")
+    db = DatabaseManager("127.0.0.1", "root" , "", "test_db")
     db.connect()
 
-    print("=== LOGIN SYSTEM ===")
-    username = input("Masukkan Username: ")
-    password = input("Masukkan Password: ")
+    print("=== LOGIN ===")
+    username = input("Username: ")
+    password = input("Password: ")
 
     if not db.login(username, password):
-        print("LOGIN GAGAL! Username atau Password salah.")
+        print("USERNAME ATAU PASSWORD SALAH!")
         db.close()
         return
-
-    print("LOGIN BERHASIL! Selamat datang di sistem klinik.")
+    print("LOGIN BERHASIL")
 
     while True:
         print("\n=== MENU ===")
-        print("1. Tambahkan Data Pasien")
-        print("2. Lihat Semua Data Pasien")
+        print("1. Tambah Data Pasien")
+        print("2. Lihat Data Pasien")
         print("3. Update Data Pasien")
         print("4. Hapus Data Pasien")
         print("5. Keluar")
 
-        pilihan = input("Pilih menu (1-5): ")
-
+        pilihan = input("Pilih Menu (1-5):")
+        
         if pilihan == '1':
-            nama = input("Nama pasien: ")
-            umur = input("Umur pasien: ")
-            penyakit = input("Penyakit yang diderita: ")
-            dokter = input("Dokter yang menangani: ")
+            print("\n === Tambah Data Pasien ===")
+            nama = input("Nama: ")
+            umur = input("Umur: ")
+            penyakit = input("Penyakt: ")
+            dokter = input("Dokter: ")
             db.create(nama, umur, penyakit, dokter)
 
         elif pilihan == '2':
+            print("\n=== Lihat Data Semua Pasien ===")
             db.read()
 
         elif pilihan == '3':
-            data_id = input("Masukkan ID pasien yang ingin diupdate: ")
+            print("\n===Upadate data pasien ===")
+            data_id = input("ID yang ingin di update: ")
             nama = input("Nama baru: ")
             umur = input("Umur baru: ")
-            penyakit = input("Penyakit baru: ")
-            dokter = input("Dokter baru: ")
+            penyakit = input("Penyakit: ")
+            dokter = input("Dokter: ")
             db.update(data_id, nama, umur, penyakit, dokter)
 
-        elif pilihan == '4':
-            data_id = input("Masukkan ID pasien yang ingin dihapus: ")
+        elif pilihan == "4":
+            print ("\n=== Hapus Data Pasien ===")
+            data_id = input("ID yang ingin di hapus: ")
             db.delete(data_id)
 
-        elif pilihan == '5':
-            print("Keluar dari program.")
+        elif pilihan == "5":
+            print("keluat dari program.")
             db.close()
             break
 
-        else:
-            print("Pilihan tidak valid. Silakan coba lagi.")
+        else: 
+            print("INPUT TIDAK VALID, SILAHKAN COBA LAGI.")
 
 if __name__ == "__main__":
     main()
