9b94efe1e387225687e418bf72bdf228a052f3ec
galangsantoso03-boop <galangsantoso03@gmail.com>
2025-10-05 22:28:32 +0700

    Update create-db-template.sql

--- belajar/create-db-template.sql
+++ belajar/create-db-template.sql
@@ -1,16 +1,20 @@
 CREATE DATABASE IF NOT EXISTS test_db;
+
 USE test_db;
 
-CREATE TABLE users (
+CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(50) NOT NULL    
 );
 
-INSERT INTO users (username, password) VALUES ('admin', '1234');
+INSERT INTO users (username, password) VALUES ("admin", "1234");
 
-CREATE TABLE data (
+
+CREATE TABLE pasien (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL
+    nama VARCHAR(50) NOT NULL,
+    umur INT NOT NULL,
+    penyakit VARCHAR(50) NOT NULL,
+    dokter VARCHAR(50) NOT NULL
 );
