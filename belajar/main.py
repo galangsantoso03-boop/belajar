9c0f9ab72946a2e7dee23761ab8a8e3f4629bb1b
galangsantoso03-boop <galangsantoso03@gmail.com>
2025-10-05 22:28:02 +0700

    Update main.py

--- belajar/main.py
+++ belajar/main.py
@@ -1,52 +1,55 @@
 from db_manager import DatabaseManager
 
 def main():
-    db = DatabaseManager("127.0.0.1" , "root", "", "test_db")
+    db = DatabaseManager("127.0.0.1", "root", "", "test_db")
     db.connect()
 
     print("=== LOGIN SYSTEM ===")
     username = input("Masukkan Username: ")
     password = input("Masukkan Password: ")
-    
+
     if not db.login(username, password):
         print("LOGIN GAGAL! Username atau Password salah.")
         db.close()
         return
-    else:
-        print("LOGIN BERHASIL!")
 
+    print("LOGIN BERHASIL! Selamat datang di sistem klinik.")
 
     while True:
         print("\n=== MENU ===")
-        print("1. Tambahkan Data")
-        print("2. Lihat Data")
-        print("3. Update Data")     
-        print("4. Hapus Data")
+        print("1. Tambahkan Data Pasien")
+        print("2. Lihat Semua Data Pasien")
+        print("3. Update Data Pasien")
+        print("4. Hapus Data Pasien")
         print("5. Keluar")
 
-        pilihan = input("pilih (1-5): ")
+        pilihan = input("Pilih menu (1-5): ")
 
         if pilihan == '1':
-            name = input("Masukan Nama: ")
-            age = input("Masukan Umur: ")
-            db.create(name, age)
-
+            nama = input("Nama pasien: ")
+            umur = input("Umur pasien: ")
+            penyakit = input("Penyakit yang diderita: ")
+            dokter = input("Dokter yang menangani: ")
+            db.create(nama, umur, penyakit, dokter)
 
         elif pilihan == '2':
             db.read()
 
         elif pilihan == '3':
-            data_id = input("Masukan ID yang ingin di update: ")
-            name = input("Masukan Nama baru: ")
-            age = input("Masukan Umur baru: ")
-            db.update(data_id, name, age)
+            data_id = input("Masukkan ID pasien yang ingin diupdate: ")
+            nama = input("Nama baru: ")
+            umur = input("Umur baru: ")
+            penyakit = input("Penyakit baru: ")
+            dokter = input("Dokter baru: ")
+            db.update(data_id, nama, umur, penyakit, dokter)
 
         elif pilihan == '4':
-            data_id = input("Masukan ID yang ingin kamu hapus: ")
+            data_id = input("Masukkan ID pasien yang ingin dihapus: ")
             db.delete(data_id)
 
         elif pilihan == '5':
             print("Keluar dari program.")
+            db.close()
             break
 
         else:
